3fbfa966b72d6d89f31c40a9141c356e5a3ac39b
wiltrouble <wiltrouble@gmail.com>
2022-08-04 19:44:27 -0400

    unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -10,14 +10,11 @@ test("matrix is filled with .", () => {
 
   for (var i = 0; i < 2; i++) {
     for (var j = 0; j < 2; j++) {
-      if (matriz[i][j] != ".")
-        empty = true;
+      if (matriz[i][j] != ".") empty = true;
     }
   }
   expect(empty).toBe(false);
 });
-
-
 
 test(`should be initializated with 4 columns and 3 rows`, () => {
   const m = new Matrix(4, 3);
@@ -25,38 +22,79 @@ test(`should be initializated with 4 columns and 3 rows`, () => {
   expect(m.rows).toBe(3);
 });
 
+test("should fill the matrix with sexteen dead cells ", () => {
+  const m = new Matrix(4, 4);
+  m.crearMatriz();
+  const matrixFilled = m.llenarMatriz();
+  let count = 0;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (matrixFilled[i][j] == ".") {
+        count++;
+      }
+    }
+  }
+  expect(count).toBe(16);
+});
 
-test('should fill the matrix with nine dead cells ', () => {
-  const m = new Matrix(3,3)
-  m.crearMatriz()
-  const matrixFilled = m.llenarMatriz()
-  let count = 0
+test("should fill the matrix with nine dead cells ", () => {
+  const m = new Matrix(3, 3);
+  m.crearMatriz();
+  const matrixFilled = m.llenarMatriz();
+  let count = 0;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      if (matrixFilled[i][j] == '.') {
-        count++
+      if (matrixFilled[i][j] == ".") {
+        count++;
       }
     }
   }
-  expect(count).toBe(9)
-})
+  expect(count).toBe(9);
+});
 
-test('should insert alive cells', () => { 
-  let aliveCells = [[1,1],[2,3],[2,4]]
-  const m = new Matrix(8,8)
-  m.crearMatriz()
-  m.llenarMatriz()
-  let count = 0
-  const matrixFilled = m.fillLiveCells(aliveCells)
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if (matrixFilled[i][j] == '*') {
-        count++
+test("should insert 3 alive cells", () => {
+  let aliveCells = [
+    [1, 1],
+    [2, 3],
+    [2, 4],
+  ];
+  const COLUMNS = 8;
+  const ROWS = 4;
+  const m = new Matrix(COLUMNS, ROWS);
+  m.crearMatriz();
+  m.llenarMatriz();
+  let count = 0;
+  const matrixFilled = m.fillLiveCells(aliveCells);
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLUMNS; j++) {
+      if (matrixFilled[i][j] == "*") {
+        count++;
       }
     }
   }
-  expect(count).toBe(3)
- })
+  expect(count).toBe(3);
+});
 
-
-
+test("should insert 4 alive cells", () => {
+  let aliveCells = [
+    [1, 1],
+    [2, 3],
+    [2, 4],
+    [3, 3]
+  ];
+  const COLUMNS = 8;
+  const ROWS = 4;
+  const m = new Matrix(COLUMNS, ROWS);
+  m.crearMatriz();
+  m.llenarMatriz();
+  let count = 0;
+  const matrixFilled = m.fillLiveCells(aliveCells);
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLUMNS; j++) {
+      if (matrixFilled[i][j] == "*") {
+        count++;
+      }
+    }
+  }
+  expect(count).toBe(4);
+});
