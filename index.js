c31016c588613126e1085e254cae279f6e3e45d5
wiltrouble <wiltrouble@gmail.com>
2022-08-04 18:10:11 -0400

    next generation

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function sum(a, b) {
-  return a + b
-}
+const Matrix = require("./models/matrix");
+
+let matriz = new Matrix(8,4);
 
 
-module.exports = sum;
+matriz.crearMatriz()
+matriz.llenarMatriz()
+let celulasVivas = [[1,4],[2,3],[2,4]]
+matriz.fillLiveCells(celulasVivas)
+console.log("________________")
+matriz.siguienteGeneracion();
--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-
-const sum = require('../index.js')
-
-
-test('should sum two numbers', () => { 
-  expect(sum(1,2)).toBe(3)
- })
