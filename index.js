4c5170c1a51b198c5d09cda2e90bdbf89917fa3f
wiltrouble <wiltrouble@gmail.com>
2022-08-04 17:18:03 -0400

    adding unit tests

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function sum(a, b) {
   return a + b
 }
+
+
+module.exports = sum;
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 
 const sum = require('../index.js')
 
+
 test('should sum two numbers', () => { 
   expect(sum(1,2)).toBe(3)
  })
